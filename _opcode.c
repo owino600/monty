955fa80981b9f3356df85d03f4c1ac2483f0cd13
owino600 <2203655@students.kcau.ac.ke>
2023-10-20 14:59:01 -0700

    update

---
+++ _opcode.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+/**
+ * __opcode - get the opcode to perform
+ *
+ * @opc: opcode sucess
+ *
+ * Return: pointer to the function that executes the opcode
+ */
+void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
+{
+	instruction_t instruct[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{"sub", _sub},
+		{"div", _div},
+		{"mul", _mul},
+		{"mod", _mod},
+		{"putchar", _putchar},
+		{"put_string", _put_string},
+		{"rotl", _rotl},
+		{"rotr", _rotr},
+		{"stack", _stack},
+		{"queue", _queue},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; instruct[i].opcode; i++)
+	{
+		if (_strcmp(instruct[i].opcode, opc) == 0)
+			break;
+	}
+
+	return (instruct[i].f);
+}
+
